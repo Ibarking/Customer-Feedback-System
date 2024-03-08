c658193b44a54be918bf08de6d4ce1a582721a8e
Ibarking <160296764+Ibarking@users.noreply.github.com>
2024-03-08 06:12:27 +0000

    asdasdasd

---
+++ htdocs/Sign-up.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign-Up</title>
+</head>
+<body>
+    <div>
+        <h1> Sign-up</h1>
+        <form action="">
+            <label for="">Phone No.</label>
+            <input type="text" name="" id="">
+            <label for="">Password</label>
+            <input type="text">
+        </form>
+    </div>
+    
+</body>
+</html>
